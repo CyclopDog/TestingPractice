871458db1c03c94214c8ff285d25fe42d12ef212
erib <eri-b@users.noreply.github.com>
2019-09-25 15:43:55 -0400

    capitalize

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,5 +1,7 @@
 const capitalize = (string) => {
-  console.log(string.toUpperCase())
+  const beg = string[0].toUpperCase()
+  const end = string.slice(1)
+  return beg + end
 }
 
 module.exports = capitalize;
